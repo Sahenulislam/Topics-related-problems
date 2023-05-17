6467650da9ab339d6e5a415df59d020f969c839e
Sahenulislam <71545997+Sahenulislam@users.noreply.github.com>
2023-05-17 23:08:18 +0600

    Add files via upload

---
+++ Combinatorics/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define INF        9223372036854775806
+#define MINF       -9223372036854775806
+#define eps          1e-8
+#define gcd __gcd
+#define yes "YES"
+#define no "NO"
+#define pb push_back
+#define si size()
+#define in " "
+#define mone "-1"
+#define zero "0"
+#define one "1"
+#define PI acos(-1)
+#define nl "\n"
+#define siz(pir) (ll)pir.size()
+const ll mod=1e9+7;
+const double pi=2*acos(0.0);
+ll lcm(ll a,ll b)
+{
+    return (a/(__gcd(a, b)))*b;
+}
+const ll mxn=1e6+10;
+
+
+int main()
+{
+    //fast
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+    }
+    return 0;
+}
